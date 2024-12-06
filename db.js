1c26af3059986f44f2f9e6fbba4706506e344174
Ricardo Silva Martins <ricardinhomartins2019@gmail.com>
2024-12-06 12:46:10 -0300

    atualização

--- db.js
+++ db.js
@@ -1,10 +1,14 @@
+require('dotenv').config()
+
+
 const { Pool } = require('pg')
 
 const BD = new Pool({
-    user: 'postgres', // nome usuario
-    host: 'localhost', // endereço do servidor
-    database: 'estoque',  // nome do banco de dados
-    password: 'admin', // senha do banco de dados
-    port: 5432, // porta de conexão do servidor
+    connectionString: process.env.DATABASE_URL
+   // user: 'postgres', // nome usuario
+    //host: 'localhost', // endereço do servidor
+    //database: 'estoque',  // nome do banco de dados
+   // password: 'admin', // senha do banco de dados
+    //port: 5432, // porta de conexão do servidor
 })
 module.exports = BD
